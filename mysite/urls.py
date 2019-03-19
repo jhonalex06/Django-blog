54cd54ac72f45e8929b99f25b0e12830c2a51273
shade <shade@pop-os.localdomain>
2019-03-19 09:04:59 -0500

    Update to django2.1.7

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', views.LoginView, name='login'),
+    url(r'^accounts/logout/$', views.LogoutView, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
     
